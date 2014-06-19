6c4783b3b2681985c741587f2ac8a0d3097792df
Felix Schüler <fschueler@posteo.de>
2014-06-19 14:24:34 +0200

    added template creation for hdfs

--- fab-extensions/src/main/scala/eu/stratosphere/fab/extensions/beans/system/hadoop/HDFS.scala
+++ fab-extensions/src/main/scala/eu/stratosphere/fab/extensions/beans/system/hadoop/HDFS.scala
@@ -2,13 +2,17 @@ package eu.stratosphere.fab.extensions.beans.system.hadoop
 
 import eu.stratosphere.fab.core.beans.system.System
 import eu.stratosphere.fab.core.beans.system.Lifespan.Lifespan
+import com.typesafe.config.{ConfigFactory, Config}
+
 
 class HDFS(lifespan: Lifespan, dependencies: Set[System] = Set()) extends System(lifespan, dependencies) {
 
-  var iteration = 0
+  final val config: Config = ConfigFactory.load("hdfs.v1.conf")
+  // TODO add fallback for own configs or merge configs
 
   def setUp(): Unit = {
     logger.info("Setting up " + toString + "...")
+    configure(config)
   }
 
   def tearDown(): Unit = {
@@ -19,6 +23,19 @@ class HDFS(lifespan: Lifespan, dependencies: Set[System] = Set()) extends System
     logger.info("Updating " + toString + "...")
     tearDown()
     setUp()
+  }
+
+  def configure(conf: Config) = {
+    scala.xml.XML.save("core-site.xml", coreSite(conf.getString("core-site.name"), conf.getString("core-site.value")))
+  }
+
+  val coreSite = { (name: String, value: String) =>
+    <configuration>
+      <property>
+        <name>{name}</name>
+        <value>{value}</value>
+      </property>
+    </configuration>
   }
 
   override def toString = "HDFS v1"
