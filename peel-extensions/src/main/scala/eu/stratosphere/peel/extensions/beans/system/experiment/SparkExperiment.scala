d9b2d0dc9f87445b353bd75ecc10dc9ae11ef13a
Felix Schüler <fschueler@posteo.de>
2014-07-08 17:02:59 +0200

    Added System and Experiment implementations for Spark.

---
+++ peel-extensions/src/main/scala/eu/stratosphere/peel/extensions/beans/system/experiment/SparkExperiment.scala
@@ -0,0 +1,84 @@
+package eu.stratosphere.peel.extensions.beans.system.experiment
+
+import eu.stratosphere.peel.core.beans.data.{ExperimentOutput, DataSet}
+import com.typesafe.config.Config
+import eu.stratosphere.peel.core.beans.experiment.Experiment
+import eu.stratosphere.peel.extensions.beans.system.spark.Spark
+import java.nio.file.{Paths, Files}
+import java.io.FileWriter
+import eu.stratosphere.peel.core.util.shell
+import spray.json._
+
+class SparkExperiment(command: String,
+                      runner: Spark,
+                      runs: Int,
+                      inputs: Set[DataSet],
+                      outputs: Set[ExperimentOutput],
+                      name: String,
+                      config: Config) extends Experiment(command, runner, runs, inputs, outputs, name, config) {
+
+  def this(runs: Int, runner: Spark, input: DataSet, output: ExperimentOutput, command: String, name: String, config: Config) = this(command, runner, runs, Set(input), Set(output), name, config)
+
+  def this(runs: Int, runner: Spark, inputs: Set[DataSet], output: ExperimentOutput, command: String, name: String, config: Config) = this(command, runner, runs, inputs, Set(output), name, config)
+
+  override def run(id: Int, force: Boolean): Experiment.Run[Spark] = new SparkExperiment.SingleJobRun(id, this, force)
+}
+
+object SparkExperiment {
+
+  case class State(name: String,
+                   command: String,
+                   var runExitCode: Option[Int] = None,
+                   var runTime: Long = 0) extends Experiment.RunState {}
+
+  object StateProtocol extends DefaultJsonProtocol with NullOptions {
+    implicit val stateFormat = jsonFormat4(State)
+  }
+
+  /**
+   * A private inner class encapsulating the logic of single run.
+   */
+  class SingleJobRun(val id: Int, val exp: SparkExperiment, val force: Boolean) extends Experiment.SingleJobRun[Spark, State] {
+
+    import eu.stratosphere.peel.extensions.beans.system.experiment.SparkExperiment.StateProtocol._
+
+    val runnerLogPath = exp.config.getString("system.spark.path.log")
+
+    override def isSuccessful = state.runExitCode.getOrElse(-1) == 0
+
+    override protected def logFilePatterns = List(s"$runnerLogPath/spark-*.out")
+
+    override protected def loadState(): State = {
+      if (Files.isRegularFile(Paths.get(s"$home/state.json"))) {
+        try {
+          io.Source.fromFile(s"$home/state.json").mkString.parseJson.convertTo[State]
+        } catch {
+          case e: Throwable => State(name, command)
+        }
+      } else {
+        State(name, command)
+      }
+    }
+
+    override protected def writeState() = {
+      val fw = new FileWriter(s"$home/state.json")
+      fw.write(state.toJson.prettyPrint)
+      fw.close()
+    }
+
+    override protected def runJob() = {
+      // try to execute the experiment run plan
+      val (runExit, t) = Experiment.time(this !(s"$command", s"$home/run.out", s"$home/run.err"))
+      state.runTime = t
+      state.runExitCode = Some(runExit)
+    }
+
+    override def cancelJob() = {}
+
+    private def !(command: String, outFile: String, errFile: String) = {
+      shell ! s"${exp.config.getString("system.spark.path.home")}/bin/spark-submit $command > $outFile 2> $errFile"
+    }
+  }
+
+}
+
