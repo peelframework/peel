30be2cd10b67e77ad5d5c814a27e4f850b46757a
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-09-25 20:52:09 +0200

    Tidy up code for `exp:run` command.

--- peel-core/src/main/scala/org/peelframework/core/cli/command/experiment/Run.scala
+++ peel-core/src/main/scala/org/peelframework/core/cli/command/experiment/Run.scala
@@ -17,14 +17,14 @@ package org.peelframework.core.cli.command.experiment
 
 import java.lang.{System => Sys}
 
+import net.sourceforge.argparse4j.impl.Arguments
+import net.sourceforge.argparse4j.inf.{Namespace, Subparser}
 import org.peelframework.core.beans.experiment.ExperimentSuite
 import org.peelframework.core.beans.system.{Lifespan, System}
 import org.peelframework.core.cli.command.Command
 import org.peelframework.core.config.{Configurable, loadConfig}
-import org.peelframework.core.graph.{Node, createGraph}
+import org.peelframework.core.graph.createGraph
 import org.peelframework.core.util.console._
-import net.sourceforge.argparse4j.impl.Arguments
-import net.sourceforge.argparse4j.inf.{Namespace, Subparser}
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 
@@ -76,123 +76,152 @@ class Run extends Command {
     val expRun = Sys.getProperty("app.suite.experiment.run").toInt
     val justRun = Sys.getProperty("app.suite.experiment.just") == "true"
 
-    logger.info(s"Running experiment '${Sys.getProperty("app.suite.experiment.name")}' from suite '${Sys.getProperty("app.suite.name")}'")
+    logger.info(s"Running experiment '$expName' from suite '$suiteName'")
 
-    val suite = context.getBean(Sys.getProperty("app.suite.name"), classOf[ExperimentSuite])
+    val suite = context.getBean(suiteName, classOf[ExperimentSuite])
     val graph = createGraph(suite)
 
     //TODO check for cycles in the graph
-    if (graph.isEmpty) throw new RuntimeException("Experiment suite is empty!")
+    if (graph.isEmpty)
+      throw new RuntimeException("Experiment suite is empty!")
 
     // find experiment
     val exps = suite.experiments.filter(_.name == expName)
-    // load config
-    for (e <- suite.experiments.filter(_.name == expName)) e.config = loadConfig(graph, e)
+
     // check if experiment exists (the list should contain exactly one element)
-    if (exps.size != 1) throw new RuntimeException(s"Experiment '$expName' either not found or ambigous in suite '$suiteName'")
+    if (exps.size != 1)
+      throw new RuntimeException(s"Experiment '$expName' either not found or ambiguous in suite '$suiteName'")
 
-    for (exp <- exps; r <- Some(exp.run(expRun, force = true))) {
+    // load config
+    for (e <- exps)
+      e.config = loadConfig(graph, e)
 
-      val allSystems = for (n <- graph.reverse.traverse(); if graph.descendants(exp).contains(n)) yield n
-      val inpSystems: Set[Node] = for (in <- exp.inputs; sys <- in.dependencies) yield sys
-      val expSystems = (graph.descendants(exp, exp.inputs) diff Seq(exp)).toSet
+    for {
+      e <- exps
+      r <- Some(e.run(expRun, force = true))
+    } {
+      // traverse all systems relevant for this experiment
+      def allSystems(reverse: Boolean) = for {
+        System(s) <- if (reverse) graph.reverse.traverse() else graph.traverse()
+        if graph.descendants(e).contains(s)
+      } yield s
+
+      // traverse all systems relevant for this experiment
+      // and required by at least one experiment input
+      def inpSystems(reverse: Boolean) = {
+        // compute the set of input descendants
+        val inpDescendant = for {
+          i <- e.inputs
+          d <- graph.descendants(i)
+        } yield d
+        allSystems(reverse = reverse).filter(inpDescendant)
+      }
+
+      // traverse all systems relevant for this experiment
+      // and not required by any experiment input
+      def expSystems(reverse: Boolean) = {
+        // compute the set of experiment, non-input descendants
+        val expDescendant = graph.descendants(e, e.inputs).toSet
+        allSystems(reverse = reverse).filter(expDescendant)
+      }
 
       try {
-        logger.info("Executing experiment '%s'".format(exp.name))
+        logger.info(s"Executing experiment '${e.name}'")
 
         // update config
-        for (n <- graph.descendants(exp)) n match {
-          case s: Configurable => s.config = exp.config
-          case _ => Unit
-        }
+        for (Configurable(c) <- graph.descendants(e))
+          c.config = e.config
 
         if (!justRun) {
+
           logger.info("Setting up / updating systems required for input data sets")
-          for (n <- inpSystems) n match {
-            case s: System => if (s.isUp) s.update() else s.setUp()
-            case _ => Unit
-          }
+          for (s <- inpSystems(reverse = true))
+            if (s.isUp) s.update()
+            else s.setUp()
 
           logger.info("Materializing experiment input data sets")
-          for (n <- exp.inputs; path = n.resolve(n.path)) if (!n.fs.exists(path)) {
-            try {
-              n.materialize()
-            } catch {
-              case e: Throwable => n.fs.rmr(path); throw e // make sure the path is cleaned for the next try
+          for {
+            n <- e.inputs
+            p = n.resolve(n.path)
+          } {
+            if (!n.fs.exists(p)) {
+              try {
+                n.materialize()
+              } catch {
+                case e: Throwable =>
+                  n.fs.rmr(p) // make sure the path is cleaned for the next try
+                  throw e
+              }
+            } else {
+              logger.info(s"Skipping already materialized path '$p'".yellow)
             }
-          } else {
-            logger.info(s"Skipping already materialized path '$path'".yellow)
           }
 
           logger.info("Tearing down redundant systems before conducting experiment runs")
-          for (n <- inpSystems diff expSystems) n match {
-            case s: System if !(Lifespan.PROVIDED :: Lifespan.SUITE :: Nil contains s.lifespan) => s.tearDown()
-            case _ => Unit
-          }
+          for {
+            s <- inpSystems(reverse = false)
+            if !(expSystems(reverse = false) contains s)
+            if !(Seq(Lifespan.PROVIDED, Lifespan.SUITE) contains s.lifespan)
+          } s.tearDown()
 
           logger.info("Setting up systems with SUITE lifespan")
-          for (n <- allSystems) n match {
-            case s: System if s.lifespan == Lifespan.SUITE && !s.isUp => s.setUp()
-            case _ => Unit
-          }
+          for {
+            s <- allSystems(reverse = true)
+            if Lifespan.SUITE == s.lifespan && !s.isUp
+          } s.setUp()
 
           logger.info("Updating systems with PROVIDED lifespan")
-          for (n <- allSystems) n match {
-            case s: System if s.lifespan == Lifespan.PROVIDED => s.update()
-            case _ => Unit
-          }
+          for {
+            s <- allSystems(reverse = true)
+            if Lifespan.PROVIDED == s.lifespan
+          } s.update()
 
           logger.info("Setting up systems with EXPERIMENT lifespan")
-          for (n <- expSystems) n match {
-            case s: System if s.lifespan == Lifespan.EXPERIMENT => s.setUp()
-            case _ => Unit
-          }
+          for {
+            System(s) <- expSystems(reverse = true)
+            if Lifespan.EXPERIMENT == s.lifespan
+          } s.setUp()
 
         } else {
           logger.info("Updating all systems")
-          for (n <- allSystems) n match {
-            case s: System => s.update()
-            case _ => Unit
-          }
+          for {
+            System(s) <- allSystems(reverse = true)
+          } s.update()
         }
 
         logger.info("Setting up systems with RUN lifespan")
-        for (n <- allSystems) n match {
-          case s: System if s.lifespan == Lifespan.RUN => s.setUp()
-          case _ => Unit
-        }
+        for {
+          s <- allSystems(reverse = true)
+          if Lifespan.RUN == s.lifespan
+        } s.setUp()
 
-        for (n <- exp.outputs) n.clean()
+        for (n <- e.outputs)
+          n.clean()
 
-        // run experiment
+        // r experiment
         r.execute()
-
         // stop for a brief intermission between runs
-        try {
-          Thread.sleep(r.exp.config.getLong("experiment.run.intermission"))
-        } catch {
-          case e: InterruptedException => // ignore interrupt
-        }
+        Thread.sleep(r.exp.config.getLong("experiment.run.intermission"))
 
       } catch {
-        case e: Throwable =>
-          logger.error(s"Exception for experiment ${exp.name} in suite ${suite.name}: ${e.getMessage}".red)
-          throw e
+        case t: Throwable =>
+          logger.error(s"Exception for experiment ${e.name} in suite ${suite.name}: ${t.getMessage}".red)
+          throw t
 
       } finally {
         if (!justRun) {
           logger.info("Tearing down systems with SUITE or EXPERIMENT lifespan")
-          for (n <- allSystems) n match {
-            case s: System if Lifespan.SUITE :: Lifespan.EXPERIMENT :: Nil contains s.lifespan => s.tearDown()
-            case _ => Unit
-          }
+          for {
+            s <- allSystems(reverse = false)
+            if Seq(Lifespan.SUITE, Lifespan.EXPERIMENT) contains s.lifespan
+          } s.tearDown()
         }
 
         logger.info("Tearing down systems with RUN lifespan")
-        for (n <- allSystems) n match {
-          case s: System if s.lifespan == Lifespan.RUN => s.tearDown()
-          case _ => Unit
-        }
+        for {
+          s <- allSystems(reverse = false)
+          if Lifespan.RUN == s.lifespan
+        } s.tearDown()
       }
     }
 
